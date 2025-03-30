c55ad43e774fc7ca4dddd1d52db6964d1a053de6
nedtrg <neddieadigwu94@gmail.com>
2025-03-30 16:26:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# python_module-week-1
+ Solution to Python Module Week 1 Assignment
---
+++ simple_calculator.py
@@ -0,0 +1,22 @@
+# Ask the user for two numbers and the operation they want to perform
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (+, -, *, /): ")
+
+# Perform the calculation based on the operation
+if operation == '+':
+    result = num1 + num2
+elif operation == '-':
+    result = num1 - num2
+elif operation == '*':
+    result = num1 * num2
+elif operation == '/':
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error! Division by zero."
+else:
+    result = "Invalid operation!"
+
+# Print the result
+print(f"The result of {num1} {operation} {num2} = {result}")
